375c5e19dea0139a98d4676ce23fb03f577c9e88
Josh <79210316+joshua-miller1@users.noreply.github.com>
2021-03-10 10:35:44 -0500

    josh update 1

---
+++ MaskMayhem (1)/Assets/playerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject Hand;
+    public weapon myWeapon;
+
+    Animator animator; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create the weapon - default to hand
+        //can expand this to include ruler? maybe a chair? idek im tired
+        myWeapon = Hand.GetComponentInChildren<weapon>();
+        animator = GetComponentInChildren<Animator>(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            DoAttack();
+            animator.SetTrigger("attackHappens");
+            //animator.ResetTrigger("attackHappens");
+        }
+    }
+
+    private void DoAttack()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, myWeapon.attackRange))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                ComplianceMeter measureMtr = hit.collider.GetComponent<ComplianceMeter>();
+                measureMtr.takeDmg(myWeapon.attackDmg);
+            }
+        }
+    }
+
+}
